d14f4ca8f6c615af16c3afa2be23447b9fc92bf6
Thiago <thiagowirzdemoura@gmail.com>
2022-01-05 12:55:07 -0300

    SignIn Page Layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
+import SignInPage from './components/SignInPage';
 import './assets/css/reset.css';
 import './assets/css/style.css';
 
@@ -9,6 +10,7 @@ export default function App(){
         <BrowserRouter>
             <Routes>
                  <Route path="/" element={ <LoginPage />} />
+                 <Route path="/cadastro" element = {<SignInPage/>}/>
             </Routes>
         </BrowserRouter>
     )
