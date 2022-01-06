9c18a459efff51cdde22be54ce55a676ec0ff8b9
Thiago <thiagowirzdemoura@gmail.com>
2022-01-06 06:13:10 -0300

    HabitsPage no habits layout

--- src/components/HabitsPage/index.js
+++ src/components/HabitsPage/index.js
@@ -1,11 +1,21 @@
-import Footer from "../Footer"
-import Header from "../Header/Header"
-import { Container } from "./style"
-export default function HabitsPage(){
-    return(
-        <Container>
-            <Header/>
-            <Footer/>
-        </Container>
-    )
+import Footer from "../Footer";
+import Header from "../Header/Header";
+import { Container, MyHabits } from "./style";
+export default function HabitsPage() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <MyHabits>
+          <p>Meus hábitos</p>
+          <div>+</div>
+        </MyHabits>
+        <p>
+          Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para
+          começar a trackear!
+        </p>
+      </Container>
+      <Footer />
+    </>
+  );
 }
--- src/components/HabitsPage/style.js
+++ src/components/HabitsPage/style.js
@@ -1,8 +1,45 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-height: 100%;
-width: 100%;
-`
+  height: 100vh;
+  width: 100%;
+  background-color: #e5e5e5;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 100px 0;
+  p {
+    font-family: 'Lexend Deca', sans-serif;
+    font-size: 18px;
+    line-height: 22px;
+    color: #666666;
+    padding: 0 20px;
+    margin-top: 22px;
+  }
+`;
 
-export {Container}
+
+const MyHabits = styled.div`
+  width: 100%;
+  height: 35px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 18px;
+  p {
+    font-size: 23px;
+    color: #126ba5;
+  }
+  div {
+    width: 40px;
+    height: 35px;
+    font-size: 27px;
+    color: #ffffff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #52b6ff;
+    border-radius: 5px;
+  }
+`;
+export { Container, MyHabits };
