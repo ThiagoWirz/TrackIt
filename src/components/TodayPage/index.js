21259ba71ca823a2a99c079a3ebaf80169294bd1
Thiago <thiagowirzdemoura@gmail.com>
2022-01-07 14:25:04 -0300

    TodayPage layout

--- src/components/TodayPage/index.js
+++ src/components/TodayPage/index.js
@@ -1,10 +1,11 @@
 import Footer from "../Footer";
 import Header from "../Header/Header";
-import { Container, Progress } from "./style";
+import { Container, Progress, Habit, CheckBox } from "./style";
 import { useContext, useEffect, useState } from "react";
 import UserContext from "../../Contexts/userContext";
 import { getTodayHabits } from "../../services/trackit";
 import dayjs from "dayjs";
+import checkmark from "../../assets/img/Vector.png"
 
 export default function TodayPage() {
     const {user, progress, setProgress} = useContext(UserContext)
@@ -26,12 +27,37 @@ export default function TodayPage() {
         promise.then(response => sethabits(response.data))
     },[])
 
+    if(habits === null){
+      return <></>
+    }
+
   return (
     <>
       <Header />
       <Container>
-          <p>{weekDays[dayjs().day()]}, {dayjs().date() < 10 ? `0${dayjs().date()}` : dayjs().date()}/{(dayjs().month() + 1) < 10 ? `0${(dayjs().month() + 1)}` : (dayjs().month() + 1)}</p>
+          <p className="title">{weekDays[dayjs().day()]}, {dayjs().date() < 10 ? `0${dayjs().date()}` : dayjs().date()}/{(dayjs().month() + 1) < 10 ? `0${(dayjs().month() + 1)}` : (dayjs().month() + 1)}</p>
           <Progress progress={progress}>{progress > 0 ? `${progress}% dos hábitos concluídos` : 'Nenhum hábito concluído ainda'}</Progress>
+        
+          {habits.map(habit =>{return(<Habit key={habit.id} >
+                                <div>
+                                    <p className="habit-name">{habit.name}</p>
+                                    <div className="habit-sequence">
+                                        <p>Sequência atual: <span
+                                            className={habit.done ? 'record' : ''}>
+                                            {habit.currentSequence} dias
+                                        </span>
+                                        </p>
+                                        <p>Seu recorde: <span
+                                            className={(habit.currentSequence === habit.highestSequence) &&
+                                                habit.done ? 'record' : ''}>
+                                            {habit.highestSequence} dias
+                                        </span>
+                                        </p>
+                                    </div>
+                                </div>
+                                <CheckBox check={habit.done}><img src={checkmark} alt="" /></CheckBox>
+                            </Habit>)})}
+                        
       </Container>
       <Footer />
     </>
--- src/components/TodayPage/style.js
+++ src/components/TodayPage/style.js
@@ -1,22 +1,62 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-height: 100%;
-padding: 100px 18px;
-background-color: #F2F2F2;
-p{
+  height: 100%;
+  padding: 100px 18px;
+  background-color: #f2f2f2;
+  .title {
     font-size: 23px;
-        line-height: 29px;
-        color: #126BA5;
-        margin-bottom: 3px;
-        font-family: "Lexend Deca", sans-serif;
-}
-`
+    line-height: 29px;
+    color: #126ba5;
+    margin-bottom: 3px;
+    font-family: "Lexend Deca", sans-serif;
+  }
+`;
 const Progress = styled.span`
-    font-size: 18px;
-    line-height: 22px;
-    color: ${props => props.progress > 0 ? '#8FC549' : '#BABABA'};
-    margin-bottom: 29px;
-`
+  font-size: 18px;
+  line-height: 22px;
+  color: ${(props) => (props.progress > 0 ? "#8FC549" : "#BABABA")};
+  margin-bottom: 29px;
+`;
 
-export{Container, Progress}
+const Habit = styled.div`
+  width: 100%;
+  height: 94px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15px;
+  background: #ffffff;
+  margin-bottom: 10px;
+  box-sizing: border-box;
+  border-radius: 7px;
+  .habit-name {
+    font-size: 20px;
+    line-height: 25px;
+    color: #666666;
+    margin-top: 13px;
+  }
+  .habit-sequence {
+    font-size: 13px;
+    line-height: 16px;
+    color: #666666;
+    margin-top: 7px;
+    .record {
+      color: #8fc549;
+    }
+  }
+`;
+
+const CheckBox = styled.div`
+  width: 69px;
+  height: 69px;
+  align-self: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${(props) => (props.check ? "#8FC549" : "#EBEBEB")};
+  border: 1px solid #e7e7e7;
+  box-sizing: border-box;
+  border-radius: 5px;
+`;
+
+export { Container, Progress, Habit, CheckBox };
